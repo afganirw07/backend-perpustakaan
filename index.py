a3f93ec4d60f8b839de635e1d79b07842d762eda
afganirw07 <afganirwansyah0707@gmail.com>
2025-11-05 15:07:35 +0700

    initial FastApi

---
+++ index.py
@@ -0,0 +1,17 @@
+# main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Look Ma, I'm deployed!"}
+
+@app.get("/api/database")
+def health_check():
+    return {"status": "good"}
+
+# This is important for Vercel
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
