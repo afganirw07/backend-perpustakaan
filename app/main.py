3ba22da6b5a24069ee44f00dc5843a01a689fb49
afganirw07 <afganirwansyah0707@gmail.com>
2025-11-07 20:05:37 +0700

    fix vercel deployment config

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
-# index.py
+# app/main.py
 from fastapi import FastAPI
 from app.routers import books
-
 
 app = FastAPI()
 
@@ -10,9 +9,3 @@ def read_root():
     return {"message": "Look Ma, I'm deployed!"}
 
 app.include_router(books.router)
-
-
-# This is important for Vercel
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
