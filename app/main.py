09d72cac05c170d0593dfcf4d9c58f3bca0e6961
afganirw07 <afganirwansyah070708@gmail.com>
2025-11-12 21:59:48 +0700

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -2,8 +2,24 @@
 from fastapi import FastAPI
 from app.routers import books, auth
 from app.service.auth import login_user
-import os
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# cors settings
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # load env
 load_dotenv()
